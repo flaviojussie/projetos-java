c4fc2a83b06e1684596aa9852eda06d7b498d3b5
flaviojussie <flvferna@gmail.com>
2016-04-15 19:30:19 -0300

    update

--- ExerciciosJava/ExerJava/src/ExerciciosAula13/Exer014.java
+++ ExerciciosJava/ExerJava/src/ExerciciosAula13/Exer014.java
@@ -1,12 +1,24 @@
 package ExerciciosAula13;
 
+import java.util.Scanner;
+
 public class Exer014 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Digite tamanho do Arquivo em MB: ");
+		float arquivo = in.nextFloat();
+		System.out.println("Digite a velocidade da internet em MB/s: ");
+		float internet = in.nextFloat();
+		// Conversão de MB para Byte.
+		float arqbyte = (float) arquivo/1024;
+		float interbyte = (float) internet*1000000/8192;
+		float tempo = (float)(arqbyte/interbyte)/60;
+		System.out.println("O tempo estimado de download é: "+tempo);
+		
 
 	}
 
