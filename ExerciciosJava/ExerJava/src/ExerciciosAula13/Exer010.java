ceaf6946080bf9bdc568266c4c449b874bacb320
flaviojussie <flvferna@gmail.com>
2016-04-15 13:09:14 -0300

    update

--- ExerciciosJava/ExerJava/src/ExerciciosAula13/Exer010.java
+++ ExerciciosJava/ExerJava/src/ExerciciosAula13/Exer010.java
@@ -9,10 +9,10 @@ public class Exer010 {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Digite a temperatura em Farenheit: ");
-		float f = in.nextFloat();
-		float c = (5 * (f-32) / 9);
-		System.out.println("A teemperatura em Celsius é: "+ c);
+		System.out.println("Digite a temperatura em Celsius: ");
+		float c = in.nextFloat();
+		float f = (c/5)*9+32;
+		System.out.println("A temperatura em Farenheit é: "+ f+"º F");
 		
 	}
 
