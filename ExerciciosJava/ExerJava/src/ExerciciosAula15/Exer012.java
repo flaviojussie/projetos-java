b67ec1235c6cfa9d5d261dda198d79f581f60151
Flavio Jussie <flvferna@gmail.com>
2016-04-23 17:44:40 -0300

    update

--- ExerciciosJava/ExerJava/src/ExerciciosAula15/Exer012.java
+++ ExerciciosJava/ExerJava/src/ExerciciosAula15/Exer012.java
@@ -5,39 +5,37 @@ import java.util.Scanner;
 public class Exer012 {
 
 	/**
+	 * Faça um Programa que pergunte em que turno você estuda. Peça
+	 * para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a
+	 * mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor
+	 * Inválido!", conforme o caso.
+	*/
+	
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int aux = Integer.MIN_VALUE;
-		
 		System.out.println("Digite o 1º número:");
-		int num1 = scan.nextInt();
+		String turno = scan.nextLine();
+		switch (turno){
+		case ("m"):
+			System.out.println("Bom dia!");
+			break;
+		case ("v"):
+			System.out.println("Boa tarde!");
+			break;
+		case ("n"):
+			System.out.println("Boa tarde!");
+			break;
+		default:
+			System.out.println("Valor Inválido!");
+			break;	
 		
-		System.out.println("Digite o 2º número:");
-		int num2 = scan.nextInt();
-		
-		System.out.println("Digite o 3º número:");
-		int num3 = scan.nextInt();
-				
-		if (num1 < num2){
-			aux = num2;		
-			num2 = num1;
-			num1 = aux;
 		}
-		if(num1 < num3){
-			aux = num3;
-			num3 = num1;
-			num1 = aux;
-		}
-		if(num2 < num3){
-			aux = num2;
-			num2 = num3;
-			num3 = aux;
-		}
-		System.out.println(num1 +" "+num2+" "+num3);
+
 	}
 
 }
